887eee55728af500a99eb8cba7c1bd6abfed5b39
Yura Boytsun <sicksneg@gmail.com>
2022-01-17 21:07:26 +0100

    Add a html card

---
+++ Card With Love/script.js
@@ -0,0 +1,29 @@
+$('document').ready();
+
+function stars()
+{
+ $(".some").css("backgroundColor","#EF89FF")
+ $("#icon").attr("src","https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/718star.svg/792px-718star.svg.png")
+ $(".content").css("background"," url(https://www.chitalnya.ru/upload3/895/731575a9d84567e4a738bc02cb6effe1.gif)")
+              .css("background-color","#CBEEFF")
+ $(".block").css("color","white")
+}
+
+function snow()
+{
+ $(".some").css("backgroundColor","#E7FFFF")
+ $("#icon").attr("src","https://naldzgraphics.net/wp-content/uploads/2013/04/4-four-Snowflake.jpg")
+ $(".content").css("background"," url(https://vignette.wikia.nocookie.net/hitlerparody/images/2/23/Snow-transparent.gif/revision/latest?cb=20151206121047.gif)")
+              .css("backgroundColor","#CBEEFF")
+ $(".block").css("color","black")
+}
+
+function leaf()
+{
+ $(".some").css("backgroundColor","#65812C");
+ $("#icon").attr("src","https://cdn4.iconfinder.com/data/icons/autumn-theme/512/maple_leaf1-512.png")
+  $(".content").css("background"," url(http://www.animationsoftware7.com/img/agifs/leaf_fall_1.gif)")
+               .css("backgroundColor","#899866")
+ $(".block").css("color","#3C5C00")
+  
+}
